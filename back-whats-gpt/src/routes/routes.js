dc08ffd1f76ec151a22cf25a4f5a479e853ddd29
Alberico Fagundes <alberico@fagundes.live>
2023-06-18 16:39:08 -0300

    criado rota teste

---
+++ back-whats-gpt/src/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+// const promptController = require("../controllers/prompt-controller");
+
+const router = express.Router();
+
+// router.post("/api/prompt", promptController.sendText);
+
+router.get("/api/test", (req, res) => {
+  return res.status(200).json({ message: "Test route is working!" });
+});
+
+module.exports = router;
--- back-whats-gpt/src/server.js
+++ back-whats-gpt/src/server.js
@@ -4,4 +4,7 @@ const port = process.env.PORT || 5000;
 
 console.log("server.js: port =>", port);
 
-app.listen(port);
+// app.listen(port);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
